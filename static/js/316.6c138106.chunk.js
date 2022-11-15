f0b4a16a1943468f98f6fba82260c65c7045cbcd
Guanlan Ji <228298909@qq.com>
2022-11-15 22:33:11 +0000

    Updates

---
+++ static/js/316.6c138106.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmovies=self.webpackChunkmovies||[]).push([[316],{1316:function(e,n,s){s.r(n);s(2791);var r=s(184);n.default=function(e){var n=e.review;return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsxs)("p",{children:["Review By: ",n.author," "]}),(0,r.jsxs)("p",{children:[n.content," "]})]})}}}]);
+//# sourceMappingURL=316.6c138106.chunk.js.map
